fb3e09dd19e6631a1216c04be218276336945324
Arnaud Héritier <aheritier@exoplatform.com>
2012-03-09 00:38:58 +0100

    Add git-svn The test with ubuntu version is erroneous. It is probably linked to the os version but also to the additional ppa or not

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,14 +3,7 @@ class git::params {
 
     case $::operatingsystem {
         /(Ubuntu)/: {
-            case $::lsbdistrelease {
-                "10.04": {
-                    $package_name   = "git-core"
-                }
-                default: {
-                    $package_name   = "git"
-                }
-            }
+            $package_name   = ["git-core", "git-svn"]
         }
         default: {
             fail ("The ${module_name} module is not supported on $::operatingsystem")
